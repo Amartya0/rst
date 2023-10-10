062a511310de00aeb2f77215b2f48738303df9e0
Amartya Jash <amartya0009@gmail.com>
2023-10-10 23:11:31 +0530

    RST for a 2d dataset and a particular target set done

--- rst.py
+++ rst.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 
 # A function that take number of objects n, number of attributes a, and number of classes d and generate a random dataset
@@ -11,7 +12,6 @@ def rstDataGenerator(n, a, d):
     # give each object an id
     id = np.arange(1, n+1).reshape(n, 1)
     dataSet = np.append(id, dataSet, axis=1)
-
     # add column names
     columnNames = []
     columnNames.append('ID')
@@ -21,14 +21,16 @@ def rstDataGenerator(n, a, d):
     columnNames.append('Class')
     dataSet = np.append([columnNames], dataSet, axis=0)
 
+    # print(dataSet)
     return dataSet
 
-# a function that takes a dataset and a set of attributes and returns the resultant indiscernibility relation set
+
 # format of dataset
 # 1st row: 0th index is ID, 1st to last-1 index is attributes, last index is class
 # 2nd row onwards: 0th index is ID/object name, 1st to last-1 index is attribute values of object ID, last index is class value of object ID
 
 
+# a function that takes a dataset and a number of attributes and returns the resultant indiscernibility relation set
 def indiscernibility(dataSet, attributeSet):
     # extract attributes from dataSet
     if attributeSet == []:
@@ -57,9 +59,105 @@ def indiscernibility(dataSet, attributeSet):
                 if flag:
                     indA.append([data[i][0], data[j][0]])
 
+    # print(indA, end='\n\n')
     return indA
 
 
-dataSet = rstDataGenerator(10, 3, 2)
-# print(dataSet)
-print(indiscernibility(dataSet, ['A1', 'A2']))
+# A function that returns the equivalence classes of a given equivalence relation set
+def eqvClasses(eqvRelSet):
+
+    eqvClasses = []
+    for i in range(len(eqvRelSet)):
+        if eqvClasses == []:
+            eqvClasses.append([eqvRelSet[i][0], eqvRelSet[i][1]])
+        else:
+            flag = True
+            for j in range(len(eqvClasses)):
+                if eqvRelSet[i][0] in eqvClasses[j]:
+                    eqvClasses[j].append(eqvRelSet[i][1])
+                    flag = False
+                    break
+                elif eqvRelSet[i][1] in eqvClasses[j]:
+                    eqvClasses[j].append(eqvRelSet[i][0])
+                    flag = False
+                    break
+            if flag:
+                eqvClasses.append([eqvRelSet[i][0], eqvRelSet[i][1]])
+
+    for i in range(len(eqvClasses)):
+        eqvClasses[i] = list(
+            map(str, sorted(list(map(int, set(eqvClasses[i]))))))
+
+    # print(eqvClasses, end='\n\n')
+    return eqvClasses
+
+
+# a function that takes a set of equivalence classes and target set and
+# returns the lower approximation of Rough Set of that target set
+def lowerApproximation(eqvClasses, targetSet):
+    lowerApprox = []
+    for i in range(len(eqvClasses)):
+        if set(eqvClasses[i]).issubset(set(targetSet)):
+            for j in range(len(eqvClasses[i])):
+                if eqvClasses[i][j] not in lowerApprox:
+                    lowerApprox.append(eqvClasses[i][j])
+    lowerApprox = list(map(str, sorted(list(map(int, set(lowerApprox))))))
+    # print(lowerApprox, end='\n\n')
+    return lowerApprox
+
+
+def upperApproximation(eqvClasses, targetSet):
+    upperApprox = []
+    for i in range(len(eqvClasses)):
+        if set(eqvClasses[i]).isdisjoint(set(targetSet)):
+            continue
+        else:
+            for j in range(len(eqvClasses[i])):
+                if eqvClasses[i][j] not in upperApprox:
+                    upperApprox.append(eqvClasses[i][j])
+    upperApprox = list(map(str, sorted(list(map(int, set(upperApprox))))))
+    # print(upperApprox, end='\n\n')
+    return upperApprox
+
+
+def rst(dataSet, noOfAttributes, targetSet):
+    eqvClassesSet = eqvClasses(indiscernibility(dataSet, noOfAttributes))
+    print("------------------------------------------------Equivalent Classes------------------------------------------------\n",
+          eqvClassesSet, end='\n\n')
+    lowerApproximationSet = lowerApproximation(eqvClassesSet, targetSet)
+    upperApproximationSet = upperApproximation(eqvClassesSet, targetSet)
+    return lowerApproximationSet, upperApproximationSet
+
+
+def ranks(sample):
+    # Return the ranks of each element in an integer sample.
+    indices = sorted(range(len(sample)), key=lambda i: sample[i])
+    return sorted(indices, key=lambda i: indices[i])
+
+
+def sample_with_minimum_distance(n, k, d):
+    # Sample of k elements from range(n), with a minimum distance d.
+    sample = random.sample(range(1, n-(k-1)*(d-1)), k)
+    return [s + (d-1)*r for s, r in zip(sample, ranks(sample))]
+
+
+n = 10
+a = 5
+dataSet = rstDataGenerator(n, a, 2)
+
+attributeSetIndex = sorted(
+    sample_with_minimum_distance(a+1, random.randint(1, a), 1))
+
+attributeSet = []
+for i in range(len(attributeSetIndex)):
+    attributeSet.append(dataSet[0][attributeSetIndex[i]])
+
+
+targetSet = list(map(str, sorted(map(int, (np.random.choice(
+    dataSet[1:, 0], random.randint(1, n), replace=False))))))
+
+print("------------------------------------------------Dataset------------------------------------------------\n", dataSet)
+print("------------------------------------------------Attribute Set------------------------------------------------\n", attributeSet)
+print("------------------------------------------------Target Set------------------------------------------------\n", targetSet)
+print("------------------------------------------------RST------------------------------------------------\n",
+      rst(dataSet, attributeSet, targetSet))
